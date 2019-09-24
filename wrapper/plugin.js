faa1c82ef136f302d570bb533d3215d022300787
Anna Parks <anna@wizehive.com>
2019-09-24 09:53:53 -0600

    chore: linting

--- wrapper/plugin.js
+++ wrapper/plugin.js
@@ -1,11 +1,7 @@
-//import '../vendor/bootstrap.js'
-//import '../vendor/bootstrap-datepicker.js'
-//import '../vendor/bootstrap-datetimepicker.js'
-
 import plugin from './lib/wrapper'
 import { Filters } from './lib/filters'
 import { Directives } from './lib/directives'
-import { znForm, ZnForm } from './lib/zn-form'
+import { ZnForm } from './lib/zn-form'
 
 Filters(plugin)
 Directives(plugin)
